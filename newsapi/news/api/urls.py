0eb949f800ab4ce97362a7186986da0eb5d71da4
a.nurmukhambetov <a.nurmukhambetov@ensoft.kz>
2021-01-24 23:27:16 +0600

    initial commit

---
+++ newsapi/news/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from news.api.views import (article_list_create_api_view, 
+                            article_detail_api_view)
+
+urlpatterns = [
+  path("articles/", article_list_create_api_view, name="article-list"),
+  path("acticles/<int:pk>/", article_detail_api_view, name="article-detail"),
+  
+]
